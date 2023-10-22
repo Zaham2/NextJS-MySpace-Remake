a5bd37b88e979a5c2a773edff4941293a20d1b9b
Zaham2 <zaham2@yahoo.com>
2023-10-22 05:33:11 +0300

    Added Navbar

---
+++ app/NavMenu.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import Image from "next/image";
+// here we import css modules
+// import styles from './NavMenu.module.css';
+
+export default function NavMenu() {
+    return (
+        <nav>
+            <Link href={'/'}>  {/* Link replaces <a> tags... Image replaces img tags */}
+                <Image          
+                    src="/favicon.ico"
+                    width={30}
+                    height={30}
+                    alt={'Some Logo'}
+                />
+            </Link>
+            <ul>
+                <li>
+                    <Link href={'/about'}>About</Link>
+                </li>
+                <li>
+                    <Link href={'/blog'}>Blog</Link>
+                </li>
+                <li>
+                    <Link href={'/users'}>Users</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
