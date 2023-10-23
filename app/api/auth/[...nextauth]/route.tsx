7245a33dc638010c9441b4d89ea7601680fa93ca
Zaham2 <zaham2@yahoo.com>
2023-10-23 09:28:39 +0300

    Added Prisma Adapter to authentication... we now have users in the db

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,9 +1,12 @@
+import { prisma } from "@/lib/prisma";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth/next";
 import GithubProvider from 'next-auth/providers/github'
 
 
 export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID!,
