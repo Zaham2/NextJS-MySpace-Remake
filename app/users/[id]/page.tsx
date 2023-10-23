c0e8f2992471260c2705588addc5fc404890548e
Zaham2 <zaham2@yahoo.com>
2023-10-23 12:33:29 +0300

    Implemented UserProfile dynamic route

---
+++ app/users/[id]/page.tsx
@@ -0,0 +1,27 @@
+import { prisma } from "@/lib/prisma";
+import { Metadata } from "next";
+
+// here would be a good place to dynamically declare metadata
+export async function generateMetadata(props: any): Promise<Metadata> {
+    const user = await prisma.user.findUnique({ where: { id: props.params.id } });
+    return { title: `User profile of ${user?.name}` };
+  }
+
+export default async function UserProfile(props: any){
+    const user = await prisma.user.findUnique({ where: {id: props.params.id }})
+    const { name, bio, image, id } = user ?? {}
+
+    return (
+        <div>
+            <h1>{name}</h1>
+
+            <img 
+                src={image!} 
+                alt={`${name}'s profile`} 
+            />
+
+            <h1>Bio</h1>
+            <p>{bio}</p>
+        </div>
+    )
+}
